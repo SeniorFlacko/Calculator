74d545067860bea4bde3cfaf9a326ea1abe71170
Cristian Aleman <jesus.aleman@globant.com>
2021-04-07 17:09:35 -0500

    circular button

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -4,8 +4,14 @@ import styles from '../theme/apppTheme';
 
 export const CalculatorScreen = () => {
   return (
-    <View>
-      <Text style={styles.texto}>Calculator works!</Text>
+    <View style={styles.calculadoraContainer}>
+      <Text style={styles.resultadoPequeno}>$1500.00</Text>
+      <Text style={styles.resultado}>$1500.00</Text>
+
+      {/** Boton  */}
+      <View style={styles.boton}>
+        <Text style={styles.botonTexto}>1</Text>
+      </View>
     </View>
   );
 };
--- src/theme/apppTheme.tsx
+++ src/theme/apppTheme.tsx
@@ -5,9 +5,39 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'black',
   },
+  calculadoraContainer: {
+    paddingHorizontal: 20,
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
   texto: {
     color: 'white',
     fontSize: 20,
+  },
+  resultado: {
+    color: 'white',
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  resultadoPequeno: {
+    fontSize: 30,
+    color: 'rgba(255,255,255,0.1)',
+    textAlign: 'right',
+  },
+  boton: {
+    height: 80,
+    width: 80,
+    backgroundColor: '#9B9B9B',
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  botonTexto: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    color: 'white',
+    fontWeight: '300',
   },
 });
 
