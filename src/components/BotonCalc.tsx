79f94dd0370f10eb1d5ecedde5f745a573cb3af8
Cristian Aleman <jesus.aleman@globant.com>
2021-04-07 18:08:33 -0500

    making button grid

--- src/components/BotonCalc.tsx
+++ src/components/BotonCalc.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import styles from '../theme/apppTheme';
 
 interface IBotonCalc {
   texto: string;
   color?: string;
+  ancho?: boolean;
 }
 
-const BotonCalc = ({texto, color = '#2D2D2D'}: IBotonCalc) => {
+const BotonCalc = ({texto, color = '#2D2D2D', ancho = false}: IBotonCalc) => {
   return (
-    <View style={[styles.boton, {backgroundColor: color}]}>
-      <Text style={styles.botonTexto}>{texto}</Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => {
+        console.log('ok');
+      }}>
+      <View
+        style={[
+          styles.boton,
+          {backgroundColor: color},
+          ancho ? {width: 180} : {width: 80},
+        ]}>
+        <Text
+          style={{
+            ...styles.botonTexto,
+            color: color === '#9B9B9B' ? 'black' : 'white',
+          }}>
+          {texto}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
